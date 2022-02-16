2be83ec2d8db1393bd35e848036729196b203259
dono118 <dono118@126.com>
2022-02-16 22:31:16 +0800

    observer

---
+++ myVue/js/observer.js
@@ -0,0 +1,23 @@
+function defineReactive(data, key, val) {
+  observe(val); // 递归遍历所有子属性
+  Object.defineProperty(data, key, {
+    enumerable: true,
+    configurable: true,
+    get: function () {
+      return val;
+    },
+    set: function (newVal) {
+      val = newVal;
+      console.log('属性' + key + '已经被监听了，现在值为：“' + newVal.toString() + '”');
+    }
+  });
+}
+
+function observe(data) {
+  if (!data || typeof data !== 'object') {
+    return;
+  }
+  Object.keys(data).forEach(function (key) {
+    defineReactive(data, key, data[key]);
+  });
+};
