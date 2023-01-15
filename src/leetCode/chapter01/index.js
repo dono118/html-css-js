0257b344914c142081b24995181732ba6bbb1779
dono118 <dono118@126.com>
2023-01-15 09:36:22 +0800

    快乐数

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -80,4 +80,47 @@ var titleToNumber = function (columnTitle) {
   return num
 }
 
-console.log(titleToNumber('ZY'))
+// console.log(titleToNumber('ZY'))
+
+/*
+  快乐数
+  编写一个算法来判断一个数 n 是不是快乐数。
+  「快乐数」 定义为：
+  对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+  然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+  如果这个过程 结果为 1，那么这个数就是快乐数。
+  如果 n 是 快乐数 就返回 true ；不是，则返回 false 。
+  示例 1：
+    输入：n = 19
+    输出：true
+    解释：
+        1^2 + 9^2 = 82
+        8^2 + 2^2 = 68
+        6^2 + 8^2 = 100
+        1^2 + 0^2 + 0^2 = 1
+  示例 2：
+    输入：n = 2
+    输出：false
+ */
+var isHappy = function (n) {
+  function getSum(i) {
+    let sum = 0
+    while (i > 0) {
+      sum += Math.pow(i % 10, 2)
+      i = Math.floor(i / 10)
+    }
+    return sum
+  }
+  while (1) {
+    if (n <= 10) {
+      if ([1, 7, 10].includes(n)) {
+        return true
+      } else {
+        return false
+      }
+    }
+    n = getSum(n)
+  }
+}
+
+console.log(isHappy(19))
