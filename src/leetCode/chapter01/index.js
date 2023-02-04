c55028b31e740b6c0709c7ec79898e257ef6dd53
dono118 <dono118@126.com>
2023-02-04 11:22:03 +0800

    同构字符串

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -123,4 +123,45 @@ var isHappy = function (n) {
   }
 }
 
-console.log(isHappy(19))
+// console.log(isHappy(19))
+
+/*
+  同构字符串
+  给定两个字符串 s 和 t ，判断它们是否是同构的。
+  如果 s 中的字符可以按某种映射关系替换得到 t ，那么这两个字符串是同构的。
+  每个出现的字符都应当映射到另一个字符，同时不改变字符的顺序。不同字符不能映射到
+  同一个字符上，相同字符只能映射到同一个字符上，字符可以映射到自己本身。
+  示例 1：
+    输入：s = "egg", t = "add"
+    输出：true
+  示例 2：
+    输入：s = "foo", t = "bar"
+    输出：false
+  示例 3：
+    输入：s = "paper", t = "title"
+    输出：true
+ */
+var isIsomorphic = function (s, t) {
+  if (s.length !== t.length) {
+    return false
+  }
+  let map1 = new Map()
+  let map2 = new Map()
+  for (let i = 0; i < s.length; i++) {
+    if (map1.has(s[i])) {
+      if (map1.get(s[i]) !== t[i]) {
+        return false
+      }
+    } else if (map2.has(t[i])) {
+      if (map2.get(t[i]) !== s[i]) {
+        return false
+      }
+    } else {
+      map1.set(s[i], t[i])
+      map2.set(t[i], s[i])
+    }
+  }
+  return true
+}
+
+console.log(isIsomorphic('paper', 'title'))
