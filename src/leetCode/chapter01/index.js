15993d639cb5847c3e7900e816c5074ba6a4516b
dono118 <dono118@126.com>
2023-02-05 10:21:06 +0800

    存在重复元素

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -164,4 +164,44 @@ var isIsomorphic = function (s, t) {
   return true
 }
 
-console.log(isIsomorphic('paper', 'title'))
+// console.log(isIsomorphic('paper', 'title'))
+
+/*
+  存在重复元素
+  给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；
+  如果数组中每个元素互不相同，返回 false 。
+  示例 1：
+    输入：nums = [1,2,3,1]
+    输出：true
+  示例 2：
+    输入：nums = [1,2,3,4]
+    输出：false
+  示例 3：
+    输入：nums = [1,1,1,3,3,4,3,2,4,2]
+    输出：true
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  // let flag = false
+  // for (let i = 0; i < nums.length; i++) {
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     if (nums[i] == nums[j]) {
+  //       flag = true
+  //     }
+  //   }
+  // }
+  // return flag
+  nums.sort()
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      return true
+    }
+  }
+  return false
+}
+
+console.log(containsDuplicate([1, 2, 3, 1]))
