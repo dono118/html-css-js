4af6acada17165561fd65adb22390ae280cc3ffd
dono118 <dono118@126.com>
2023-02-12 09:58:54 +0800

    存在重复元素 II

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -204,4 +204,35 @@ var containsDuplicate = function (nums) {
   return false
 }
 
-console.log(containsDuplicate([1, 2, 3, 1]))
+// console.log(containsDuplicate([1, 2, 3, 1]))
+
+/*
+  存在重复元素 II
+  给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，
+  满足 nums[i] == nums[j] 且 abs(i - j) <= k 。如果存在，返回 true ；否则，返回 false 。
+  示例 1：
+    输入：nums = [1,2,3,1], k = 3
+    输出：true
+  示例 2：
+    输入：nums = [1,0,1,1], k = 1
+    输出：true
+  示例 3：
+    输入：nums = [1,2,3,1,2,3], k = 2
+    输出：fale
+ */
+var containsNearbyDuplicate = function (nums, k) {
+  let map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i]) && i - map.get(nums[i]) <= k) {
+      return true
+    } else {
+      map.set(nums[i], i)
+    }
+  }
+  return false
+}
+
+let nums = [1, 2, 3, 1],
+  k = 3
+
+console.log(containsNearbyDuplicate(nums, k))
