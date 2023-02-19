2ddd8efc37a99c885c92da0bde6ffc1dbacc8197
dono118 <dono118@126.com>
2023-02-19 10:04:56 +0800

    用队列实现栈

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -232,7 +232,91 @@ var containsNearbyDuplicate = function (nums, k) {
   return false
 }
 
-let nums = [1, 2, 3, 1],
-  k = 3
+// let nums = [1, 2, 3, 1],
+//   k = 3
 
-console.log(containsNearbyDuplicate(nums, k))
+// console.log(containsNearbyDuplicate(nums, k))
+
+/*
+  用队列实现栈
+  请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+  实现 MyStack 类：
+  void push(int x) 将元素 x 压入栈顶。
+  int pop() 移除并返回栈顶元素。
+  int top() 返回栈顶元素。
+  boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
+  示例：
+    输入：
+      ["MyStack", "push", "push", "top", "pop", "empty"]
+      [[], [1], [2], [], [], []]
+    输出：
+      [null, null, null, 2, 2, false]
+    解释：
+      MyStack myStack = new MyStack();
+      myStack.push(1);
+      myStack.push(2);
+      myStack.top(); // 返回 2
+      myStack.pop(); // 返回 2
+      myStack.empty(); // 返回 False
+
+ */
+/**
+ * Initialize your data structure here.
+ */
+var MyStack = function () {
+  this.queue = []
+}
+
+/**
+ * Push element x onto stack.
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function (x) {
+  return this.queue.push(x)
+}
+
+/**
+ * Removes the element on top of the stack and returns that element.
+ * @return {number}
+ */
+MyStack.prototype.pop = function () {
+  return this.queue.pop()
+}
+
+/**
+ * Get the top element.
+ * @return {number}
+ */
+MyStack.prototype.top = function () {
+  return this.queue[this.queue.length - 1]
+}
+
+/**
+ * Returns whether the stack is empty.
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function () {
+  return this.queue.length === 0
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
+
+let obj = new MyStack()
+obj.push(1)
+obj.push(2)
+let e1 = obj.pop()
+console.log(e1, obj.queue)
+let e2 = obj.top()
+console.log(e2, obj.queue)
+let e3 = obj.pop()
+console.log(e3, obj.queue)
+let bool = obj.empty()
+console.log(bool, obj.queue)
