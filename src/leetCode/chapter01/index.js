28d4617553ed0a778badf8557673704d40564064
dono118 <dono118@126.com>
2023-03-05 10:05:40 +0800

    丑数

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -364,4 +364,45 @@ var addDigits = function (num) {
   return ((num - 1) % 9) + 1
 }
 
-console.log(addDigits(38))
+// console.log(addDigits(38))
+
+/*
+  丑数
+  丑数 就是只包含质因数 2、3 和 5 的正整数。
+  给你一个整数 n ，请你判断 n 是否为 丑数 。如果是，返回 true ；否则，返回 false 。
+  示例 1：
+    输入：n = 6
+    输出：true
+    解释：6 = 2 x 3
+      
+  示例 2：
+    输入：n = 1
+    输出：true
+    解释：1 没有质因数，因此它的全部质因数是 {2, 3, 5} 的空集。习惯上将其视作第一个丑数。
+
+  示例 3：
+    输入：n = 14
+    输出：fale
+    解释：14 不是丑数，因为它包含了另外一个质因数 7 。
+ */
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function (n) {
+  while (n > 1) {
+    if (n % 2 == 0) {
+      n /= 2
+    } else if (n % 3 == 0) {
+      n /= 3
+    } else if (n % 5 == 0) {
+      n /= 5
+    } else {
+      return false
+    }
+  }
+  return n == 1 ? true : false
+}
+
+console.log(isUgly(8))
