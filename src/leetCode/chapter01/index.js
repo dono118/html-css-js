d2d638b48774c0d507062c91f3bc8eb347254d7b
dono118 <dono118@126.com>
2023-03-18 10:59:01 +0800

    移动零

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -450,4 +450,35 @@ var missingNumber = function (nums) {
   return nLen
 }
 
-console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
+// console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
+
+/*
+  移动零
+  给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+  请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+  示例 1:
+    输入: nums = [0,1,0,3,12]
+    输出: [1,3,12,0,0]
+  示例 2:
+    输入: nums = [0]
+    输出: [0]
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var moveZeroes = function (nums) {
+  for (let i = 0, j = -1, len = nums.length; i < len; i++) {
+    if (nums[i] === 0) {
+      if (j < 0) {
+        j = i
+      }
+    }
+    if (nums[i] !== 0 && j >= 0) {
+      ;[nums[j], nums[i]] = [nums[i], nums[j]]
+      j++
+    }
+  }
+  return nums
+}
+console.log(moveZeroes([0, 1, 0, 3, 12]))
