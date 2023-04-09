c4bea2a8786611edc9a766ae2d40640c8a3a04b0
dono118 <dono118@126.com>
2023-04-09 09:51:22 +0800

    3 的幂

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -579,4 +579,29 @@ var isPowerOfTwo = function (n) {
   return n === 1
 }
 
-console.log(isPowerOfTwo(16))
+// console.log(isPowerOfTwo(16))
+
+/*
+  3 的幂
+  给定一个整数，写一个函数来判断它是否是 3 的幂次方。如果是，返回 true ；
+  否则，返回 false 。
+  整数 n 是 3 的幂次方需满足：存在整数 x 使得 n == 3^x
+  示例 1:
+    输入：n = 27
+    输出：true
+  示例 2:
+    输入: n = 0
+    输出: false
+  示例 3:
+    输入: n = 9
+    输出: true
+ */
+var isPowerOfThree = function (n) {
+  if (n < 1) return false
+  while (n % 3 === 0) {
+    n /= 3
+  }
+  return n === 1
+}
+
+console.log(isPowerOfThree(45))
