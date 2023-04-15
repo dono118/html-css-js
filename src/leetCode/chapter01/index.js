8136ea75a8bbc56c29716108788a0a89ec706ed4
dono118 <dono118@126.com>
2023-04-15 11:47:43 +0800

    比特位计数

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -604,4 +604,47 @@ var isPowerOfThree = function (n) {
   return n === 1
 }
 
-console.log(isPowerOfThree(45))
+// console.log(isPowerOfThree(45))
+
+/*
+  比特位计数
+  给你一个整数 n ，对于 0 <= i <= n 中的每个 i ，计算其二进制表示中 1 的个数 ，
+  返回一个长度为 n + 1 的数组 ans 作为答案。
+  示例 1:
+    输入：n = 2
+    输出：[0,1,1]
+    解释：
+      0 --> 0
+      1 --> 1
+      2 --> 10
+  示例 2:
+    输入: n = 5
+    输出: [0,1,1,2,1,2]
+    解释：
+      0 --> 0
+      1 --> 1
+      2 --> 10
+      3 --> 11
+      4 --> 100
+      5 --> 101
+ */
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function (n) {
+  const stk = []
+  for (let i = 0; i <= n; i++) {
+    let b = 0
+    let a = i.toString(2)
+    for (let j = 0; j < a.length; j++) {
+      if (a[j] == 1) {
+        b++
+      }
+    }
+    stk.push(b)
+  }
+  return stk
+}
+
+console.log(countBits(5))
