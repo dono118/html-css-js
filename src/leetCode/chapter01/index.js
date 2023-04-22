02dd660c93dde38729f77ab1f60e49b5dd25ed7d
dono118 <dono118@126.com>
2023-04-22 10:32:02 +0800

    4 的幂

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -647,4 +647,25 @@ var countBits = function (n) {
   return stk
 }
 
-console.log(countBits(5))
+// console.log(countBits(5))
+
+/*
+  4 的幂
+  给定一个整数，写一个函数来判断它是否是 4 的幂次方。如果是，返回 true ；
+  否则，返回 false 。
+  整数 n 是 4 的幂次方需满足：存在整数 x 使得 n == 4^x
+  示例 1:
+    输入：n = 16
+    输出：true
+  示例 2:
+    输入: n = 5
+    输出: false
+  示例 3:
+    输入: n = 1
+    输出: true
+ */
+var isPowerOfFour = function (n) {
+  return n > 0 && (n & (n - 1)) == 0 && n % 3 == 1
+}
+
+console.log(isPowerOfFour(16))
