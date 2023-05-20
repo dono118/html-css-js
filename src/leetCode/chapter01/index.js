817c8267057b894baf6ec1ef10a0cc8f8392c78c
dono118 <dono118@126.com>
2023-05-20 10:02:01 +0800

    两个数组的交集 Ⅱ

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -769,4 +769,50 @@ var intersection = function (nums1, nums2) {
   return nums3
 }
 
-console.log(intersection([1, 2, 2, 1], [2, 2]))
+// console.log(intersection([1, 2, 2, 1], [2, 2]))
+
+/*
+  两个数组的交集 Ⅱ
+  给你两个整数数组 nums1 和 nums2 ，请你以数组形式返回两数组的交集。
+  返回结果中每个元素出现的次数，应与元素在两个数组中都出现的次数一致
+  （如果出现次数不一致，则考虑取较小值）。可以不考虑输出结果的顺序 。
+  示例 1:
+    输入：nums1 = [1,2,2,1], nums2 = [2,2]
+    输出：[2,2]
+  示例 2:
+    输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+    输出: [9,4]
+    解释：[4,9] 也是可通过的
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function (nums1, nums2) {
+  let newArr = [],
+    i = 0,
+    j = 0,
+    len1 = nums1.length,
+    len2 = nums2.length
+
+  if (len1 === 0 || len2 === 0) return newArr
+
+  nums1.sort((a, b) => a - b)
+  nums2.sort((a, b) => a - b)
+
+  while (i < len1 && j < len2) {
+    if (nums1[i] === nums2[j]) {
+      newArr.push(nums1[i])
+      i++
+      j++
+    } else if (nums1[i] > nums2[j]) {
+      j++
+    } else {
+      i++
+    }
+  }
+  return newArr
+}
+
+console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4]))
