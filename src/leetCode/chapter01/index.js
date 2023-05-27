4f49d2f1e94ef24cd6ffe2bd5654bfabfeee467e
dono118 <dono118@126.com>
2023-05-27 11:24:22 +0800

    有效的完全平方数

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -815,4 +815,37 @@ var intersect = function (nums1, nums2) {
   return newArr
 }
 
-console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4]))
+// console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4]))
+
+/*
+  有效的完全平方数
+  给你一个正整数 num 。如果 num 是一个完全平方数，则返回 true ，否则返回 false 。
+  完全平方数 是一个可以写成某个整数的平方的整数。换句话说，它可以写成某个整数和自身的乘积。
+  不能使用任何内置的库函数，如  sqrt 。
+  示例 1：
+    输入：num = 16
+    输出：true
+    解释：返回 true ，因为 4 * 4 = 16 且 4 是一个整数。
+  示例 2：
+    输入：num = 14
+    输出：false
+    解释：返回 false ，因为 3.742 * 3.742 = 14 但 3.742 不是一个整数。
+ */
+/**
+ * @param {Number} num
+ * @return {Boolean}
+ */
+var isPerfectSquare = function (num) {
+  if (num === 1) return true
+  let left = 1,
+    right = num
+  while (left <= right) {
+    let mid = (left + right) >> 1
+    if (mid * mid === num) return true
+    else if (mid * mid > num) right = mid - 1
+    else left = mid + 1
+  }
+  return false
+}
+
+console.log(isPerfectSquare(14))
