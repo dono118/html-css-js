47a3c8921fcd2b2b2b7209d3547d6d8694908fca
dono118 <dono118@126.com>
2023-06-03 10:43:58 +0800

    字符串中的第一个唯一字符

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -848,4 +848,33 @@ var isPerfectSquare = function (num) {
   return false
 }
 
-console.log(isPerfectSquare(14))
+// console.log(isPerfectSquare(14))
+
+/*
+  字符串中的第一个唯一字符
+  给定一个字符串 s ，找到它的第一个不重复的字符，并返回它的索引 。如果不存在，则返回 -1 。
+  示例 1：
+    输入：s = "leetcode"
+    输出：0
+  示例 2：
+    输入：s = "loveleetcode"
+    输出：2
+  示例 3：
+    输入：s = "aabb"
+    输出：-1
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+  for (let i = 0; i < s.length; i++) {
+    if (s.lastIndexOf(s[i]) == s.indexOf(s[i])) {
+      let a = s.indexOf(s[i])
+      return a
+    }
+  }
+  return -1
+}
+
+console.log(firstUniqChar('loveleetcode'))
