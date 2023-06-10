afe1551372fcd8fe7c505d1d25c351b4f9655ccb
dono118 <dono118@126.com>
2023-06-10 10:25:20 +0800

    最长回文串

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -877,4 +877,40 @@ var firstUniqChar = function (s) {
   return -1
 }
 
-console.log(firstUniqChar('loveleetcode'))
+// console.log(firstUniqChar('loveleetcode'))
+
+/*
+  最长回文串
+  给定一个包含大写字母和小写字母的字符串 s ，返回 通过这些字母构造成的 最长的回文串 。
+  在构造过程中，请注意 区分大小写 。比如 "Aa" 不能当做一个回文字符串。
+  示例 1：
+    输入：s = "abccccdd"
+    输出：7
+    解释：我们可以构造的最长的回文串是"dccaccd", 它的长度是 7。
+  示例 2：
+    输入：s = "a"
+    输出：1
+  示例 3：
+    输入：s = "aaaaaccc"
+    输出：7
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function (s) {
+  s = s.split('')
+  let map = new Map()
+  let result = 0
+  for (let i = 0; i < s.length; i++) {
+    if (map.get(s[i]) === 1) {
+      map.delete(s[i])
+      result += 2
+    } else {
+      map.set(s[i], 1)
+    }
+  }
+  return map.size ? result + 1 : result
+}
+
+console.log(longestPalindrome('aaaaaccc'))
