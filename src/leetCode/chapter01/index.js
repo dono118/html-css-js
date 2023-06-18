3afbe3eec676c2757562f98010a035b57714f96c
dono118 <dono118@126.com>
2023-06-18 10:50:33 +0800

    Fizz Buzz

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -913,4 +913,43 @@ var longestPalindrome = function (s) {
   return map.size ? result + 1 : result
 }
 
-console.log(longestPalindrome('aaaaaccc'))
+// console.log(longestPalindrome('aaaaaccc'))
+
+/*
+  Fizz Buzz
+  给你一个整数 n ，找出从 1 到 n 各个整数的 Fizz Buzz 表示，并用字符串数组 answer（下标从 1 开始）返回结果，其中：
+  answer[i] == "FizzBuzz" 如果 i 同时是 3 和 5 的倍数。
+  answer[i] == "Fizz" 如果 i 是 3 的倍数。
+  answer[i] == "Buzz" 如果 i 是 5 的倍数。
+  answer[i] == i （以字符串形式）如果上述条件全不满足。
+  示例 1：
+    输入：n = 3
+    输出：["1","2","Fizz"]
+  示例 2：
+    输入：n = 5
+    输出：["1","2","Fizz","4","Buzz"]
+  示例 3：
+    输入：n = 15
+    输出：["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ */
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+  var arr = []
+  for (var i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      arr.push('FizzBuzz')
+    } else if (i % 3 == 0) {
+      arr.push('Fizz')
+    } else if (i % 5 == 0) {
+      arr.push('Buzz')
+    } else {
+      arr.push(i.toString())
+    }
+  }
+  return arr
+}
+
+console.log(fizzBuzz(15))
