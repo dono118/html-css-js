7feffba53f97d578d1c9ebcfc9f5c0faf6adb29f
dono118 <dono118@126.com>
2023-07-02 09:51:36 +0800

    字符串相加

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -980,4 +980,42 @@ var thirdMax = function (nums) {
   return arr.length >= 3 ? arr[2] : arr[0]
 }
 
-console.log(thirdMax([1, 2, 3, 2, 5]))
+// console.log(thirdMax([1, 2, 3, 2, 5]))
+
+/*
+  字符串相加
+  给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和并同样以字符串形式返回。
+  你不能使用任何內建的用于处理大整数的库（比如 BigInteger）， 也不能直接将输入的字符串转换为整数形式。
+  示例 1：
+    输入：num1 = "11", num2 = "123"
+    输出："134"
+  示例 2：
+    输入：num1 = "456", num2 = "77"
+    输出："533"
+  示例 3：
+    输入：num1 = "0", num2 = "0"
+    输出："0"
+ */
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function (num1, num2) {
+  let l = num1.length - 1
+  let r = num2.length - 1
+  let carry = 0
+  let ans = []
+  while (l >= 0 || r >= 0 || carry !== 0) {
+    let c1 = l >= 0 ? num1.charAt(l) - '0' : 0
+    let c2 = r >= 0 ? num2.charAt(r) - '0' : 0
+    let sum = c1 + c2 + carry
+    ans.push(sum % 10)
+    carry = Math.floor(sum / 10)
+    l--
+    r--
+  }
+  return ans.reverse().join('')
+}
+
+console.log(addStrings('123', '45'))
