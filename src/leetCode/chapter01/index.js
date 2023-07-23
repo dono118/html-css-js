3b30c302d3466f85ce133086d82afdc1ee06e8a8
dono118 <dono118@126.com>
2023-07-23 09:27:43 +0800

    找到所有数组中消失的数字

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -1052,4 +1052,38 @@ var countSegments = function (s) {
   return count
 }
 
-console.log(countSegments('Hello, my name is John'))
+// console.log(countSegments('Hello, my name is John'))
+
+/*
+  找到所有数组中消失的数字
+  给你一个含 n 个整数的数组 nums ，其中 nums[i] 在区间 [1, n] 内。请你找出所有
+  在 [1, n] 范围内但没有出现在 nums 中的数字，并以数组的形式返回结果。
+  示例 1：
+    输入：nums = [4,3,2,7,8,2,3,1]
+    输出：[5,6]
+  示例 2：
+    输入：nums = [1,1]
+    输出：[2]
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function (nums) {
+  let len = nums.length,
+    res = []
+  for (let i = 0; i < len; i++) {
+    let val = Math.abs(nums[i]) - 1
+    if (nums[val] > 0) {
+      nums[val] = -nums[val]
+    }
+  }
+  for (let i = 0; i < len; i++) {
+    if (nums[i] > 0) {
+      res.push(i + 1)
+    }
+  }
+  return res
+}
+
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]))
