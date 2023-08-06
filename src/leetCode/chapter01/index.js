e11a7c5f2f2390963410cff4b069f13dc3265784
dono118 <dono118@126.com>
2023-08-06 09:41:24 +0800

    数字的补数

--- src/leetCode/chapter01/index.js
+++ src/leetCode/chapter01/index.js
@@ -1117,4 +1117,36 @@ var repeatedSubstringPattern = function (s) {
   return false
 }
 
-console.log(repeatedSubstringPattern('abcabcabcabc'))
+// console.log(repeatedSubstringPattern('abcabcabcabc'))
+
+/*
+  数字的补数
+  对整数的二进制表示取反（0 变 1 ，1 变 0）后，再转换为十进制表示，可以得到这个整数的补数。
+  例如，整数 5 的二进制表示是 "101" ，取反后得到 "010" ，再转回十进制表示得到补数 2 。
+  给你一个整数 num ，输出它的补数。
+  示例 1：
+    输入：num = 5
+    输出：2
+    解释：5 的二进制表示为 101（没有前导零位），其补数为 010。所以你需要输出 2 。
+  示例 2：
+    输入：num = 1
+    输出：0
+    解释：1 的二进制表示为 1（没有前导零位），其补数为 0。所以你需要输出 0 。
+ */
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function (num) {
+  let str = num.toString(2)
+  let len = str.length,
+    sum = 0
+  for (let i = 0; i < len; i++) {
+    if (str[i] === '0') {
+      sum += 2 ** (len - i - 1)
+    }
+  }
+  return sum
+}
+
+console.log(findComplement(4))
